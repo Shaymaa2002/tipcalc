6adfc96c7a1e02fd81c76a7eaac082e5aaf1dd0e
Shaymaa Hasan <hasashay@shernet.sheridancollege.ca>
2025-03-30 19:09:09 -0400

    Completed Tip Calculator Angular app with input and output components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { InputComponent } from './pages/input/input.component';
+import { OutputComponent } from './pages/output/output.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: InputComponent },
+  { path: 'output', component: OutputComponent }
+];
